70f827b9a41dcec90dcea2d84ee4ca99798d3a4a
Matthew Bilton <mabilton@DESKTOP-P9MM3TE.localdomain>
2022-01-24 22:09:40 +1300

    Simplified indexing and setting; added update and filter methods.

--- arraytainers/base/getset.py
+++ arraytainers/base/getset.py
@@ -19,6 +19,18 @@ class GetterMixin:
         if tuple_keys:
             raise KeyError(f'Cannot use the tuples {", ".join(tuple_keys)} as key(s) in an Arraytainer, since tuples are used to extract multiple elements.')
 
+    def filter(self, to_keep, *key_iterable):
+        if key_iterable:
+            key_iterable = list(key_iterable)
+            key_i = key_iterable.pop(0)
+            filtered = self.copy()
+            filtered[key_i] = filtered[key_i].filter(to_keep, *key_iterable)
+            return filtered
+        else:
+            filtered = {key: self[key] for key in to_keep}
+            filtered = list(filtered.values()) if self._type is list else filtered
+            return self.__class__(filtered)
+
     def get(self, *key_iterable):
         key_iterable = list(key_iterable)
         key_i = key_iterable.pop(0)
@@ -28,46 +40,27 @@ class GetterMixin:
             return self.contents[key_i]
 
     def __getitem__(self, key):
-        if self.is_container(key):
+        if self.is_array(key) or is_slice(key):
+            item = self._index_with_array_or_slice(key)
+        elif self.is_container(key):
             item = self._index_with_container(key)
-        # Interpret indexing with list/dict as a container:
-        # elif isinstance(key, (dict, list)):
-        #     key = self.__class__(key)
-        #     item = self._index_with_container(key)
-        elif self.is_array(key):
-            item = self._index_with_array(key)
-        elif is_slice(key):
-            item = self._index_with_slices(key)
-        # Treat tuple which isn't a slice as a tuple of non-array keys:
-        elif isinstance(key, (tuple, list, dict)):
-            if isinstance(key, dict):
-                item = {key_i: self[key_i][val_i] for key_i, val_i in key.items()}
-            else:
-                item = {key_i: self[key_i] for key_i in key}
-            item = list(item.values()) if self._type is list else item
-            item = self.__class__(item)
+        # If given iterable, attempt to interpret it as an arraytainer:
+        elif isinstance(key, (dict, list, tuple)):
+            key = self.__class__(key)
+            item = self._index_with_container(key)
         else:
             item = self._index_with_hash(key)
         return item
 
-    def _index_with_container(self, key_container):
-        item = self.copy()
-        for container_key in self.keys():
-            array_key = key_container[container_key]  
-            item[container_key] = self.contents[container_key][self.array(array_key)]
-        return item
+    def _index_with_container(self, key_container):        
+        item = {key: self.contents[key][val] for key, val in key_container.items()}
+        item = list(item.values()) if self._type is list else item
+        return self.__class__(item)
 
-    def _index_with_array(self, array_key):
-        item = self.copy()
-        for container_key in self.keys():
-            item[container_key] = self.contents[container_key][self.array(array_key)]
-        return item
-
-    def _index_with_slices(self, slices):
-        item = self.copy()
-        for container_key in self.keys():
-            item[container_key] = self.contents[container_key][slices]
-        return item
+    def _index_with_array_or_slice(self, array_or_slice):
+        item = {key: self.contents[key][array_or_slice] for key in self.keys()}
+        item = list(item.values()) if self._type is list else item
+        return self.__class__(item)
 
     def _index_with_hash(self, key):
         try:
@@ -78,91 +71,50 @@ class GetterMixin:
             raise KeyError(' '.join(error_msg))
         return item
 
-def _attempt_append(contents, new_val):
-    try:
-        contents.append(new_val)
-    except AttributeError:
-        error_msg = ('Unable to append values to dictionary-like container;',
-                    "use 'my_arraytainer[new_key] = new_value' or the my_arraytainer.set method instead.")
-        raise AttributeError(' '.join(error_msg))
-
 class SetterMixin:
-    
-    def append(self, new_val, key_iterable=()):
+
+    def update(self, new_val, *key_iterable):
         if key_iterable:
             key_iterable = list(key_iterable)
             key_i = key_iterable.pop(0)
-            if key_iterable:
-                self[key_i].append(new_val, key_iterable)
-            else:
-                _attempt_append(self.contents[key_i], new_val)
+            self[key_i].update(new_val, *key_iterable)
         else:
-            _attempt_append(self.contents, new_val) 
-
-    # def set(self, new_val, *key_iterable):
-
-    #     if not key_iterable:
-    #         raise KeyError('Must specify at least one key when using the set method.')
-
-    #     # If key_iterable is provided by the user as a list/tuple:
-    #     if len(key_iterable) == 1 and isinstance(key_iterable[0], (tuple,list)):
-    #         key_iterable = key_iterable[0]
-            
-    #     key_iterable = list(key_iterable)
-    #     key_i = key_iterable.pop(0)
-
-    #     if key_iterable:
-    #         self[key_i].set(new_val, *key_iterable)
-    #     else:
-    #         try:
-    #             self[key_i] = new_val
-    #         except AttributeError:
-    #             error_msg = ("Unable to new assign items to a list-like container;",
-    #                          "use the append method instead.")
-    #             raise AttributeError(' '.join(error_msg))
+            new_val = self.__class__(new_val)
+            if self._type is dict:
+                self.contents.update(new_val)
+            else:
+                self.contents.append(new_val)
 
     def __setitem__(self, key, new_value):
 
-        new_value = self._preprocess_set_new_value(new_value)
+        # Ensure new_value is either an arraytainer or an array:
+        if isinstance(new_value, Number):
+            new_value = self.array(new_value)
+        elif not self.is_array(new_value):
+            new_value = self.__class__(new_value) 
         
-        if self.is_container(key):
+        if self.is_array(key) or is_slice(key):
+            self._set_with_array_or_slice(key, new_value)
+        elif self.is_container(key):
             self._set_with_container(key, new_value)
         # Interpret indexing with list/dict as a container:
-        elif isinstance(key, (dict,list)):
+        elif isinstance(key, (dict, list, tuple)):
             key = self.__class__(key)
             item = self._set_with_container(key, new_value)
-        elif self.is_array(key):
-            self._set_with_array(key, new_value)
-        elif is_slice(key):
-            self._set_with_slices(key, new_value)
         else:
-            self._set_with_hash(key, new_value)    
+            self._set_with_hash(key, new_value) 
 
-    def _preprocess_set_new_value(self, new_val):
-        if isinstance(new_val, Number):
-            new_val = self.array(new_val)
-        elif not self.is_array(new_val):
-            new_val = self.__class__(new_val) 
-        return new_val
-
-    def _set_with_container(self, container_key, new_value):
-        for key in self.keys():
-            value_i = new_value[key] if self.is_container(new_value) else new_value
-            idx_i = container_key[key]
-            if self.is_array(self[key]):
-                self._set_array_item(key, idx_i, value_i)
+    def _set_with_container(self, key_container, new_value):
+        for key, val in key_container.items():
+            if self.is_array(self[key][val]):
+                self._set_array_item(key, val, new_value)
             else:
-                self[key][idx_i] = value_i
+                self[key][val] = new_value[key] if self.is_container(new_value) else new_value
 
-    def _set_with_array(self, array_key, new_value):
+    def _set_with_array_or_slice(self, array_or_slice, new_value):
         for container_key in self.keys():
             value_i = new_value[container_key] if self.is_container(new_value) else new_value
-            self._set_array_item(container_key, array_key, value_i)
-
-    def _set_with_slices(self, slices, new_value):
-        for container_key in self.keys():
-            value_i = new_value[container_key] if self.is_container(new_value) else new_value
-            self._set_array_item(container_key, slices, value_i)
+            self._set_array_item(container_key, array_or_slice, value_i)
 
     def _set_array_item(self, key, idx, value_i):
         error_msg = ('Cannot use array to set value with base Arraytainer class;', 
--- tests/main_tests/test_setting.py
+++ tests/main_tests/test_setting.py
@@ -32,58 +32,58 @@ class SetMixin:
         utils.set_contents_item(contents_copy, key_iterable, new_val)
         utils.assert_equal_values(arraytainer.unpacked, contents)
     
-    HASH_SET_VALUES_TUPLES = tuple(tuple([x]) if isinstance(x, tuple) else x for x in HASH_SET_VALUES)
-    SET_METHOD_TEST_CASES = {
-        'list': cartesian_prod( [[(2,3),(3,2)],(2,2),[(2,1),[(2,2),(3,2)]]],
-                                HASH_SET_VALUES_TUPLES,
-                              ( ((0,),None), ((0,1),None), ((1,),None), ((2,1),None), ((2,1,1),None),
-                                ((3,), 'key_error'), ((2,0,0),AttributeError), ((), 'key_error') ) ),
-        'dict': cartesian_prod( {'a':{'a':(2,1),'c':(2,2)},'b':{'d':(3,2),'b':(2,2)}},
-                                HASH_SET_VALUES_TUPLES,
-                              ( (('a',), None), (('b',), None), (('c',), None), (('a','a'),None), (('a','e'),None),
-                                ((0,), None), ((0,0), 'key_error'),  (('a','a','a'), AttributeError), ((), 'key_error') ) ),
-        'mixed': cartesian_prod( [{'a':[(2,2),{'c':(2,1)},(1,2)],'b':(2,1)},{'b':(2,1),'c':[(2,2),(1,2)]}], 
-                                 HASH_SET_VALUES_TUPLES,
-                              ( ((0,), None), ((1,), None), ((0,'a'), None), ((0,'a',1,'c'), None), ((1,'c',0), None),
-                                ((0,'b'), None), ((0,'a',3), 'key_error'), ((), 'key_error') ) )
-    }
-    SET_METHOD_TEST_CASES = utils.group_first_n_params(SET_METHOD_TEST_CASES, n=2)
-    @pytest.mark.parametrize('contents_list, key_iterable, exception', utils.unpack_test_cases(SET_METHOD_TEST_CASES), 
-                                                                       ids=utils.unpack_test_ids(SET_METHOD_TEST_CASES), 
-                                                                       indirect=['contents_list'])
-    def test_set_and_copy_method(self, contents_list, key_iterable, exception):
+    # HASH_SET_VALUES_TUPLES = tuple(tuple([x]) if isinstance(x, tuple) else x for x in HASH_SET_VALUES)
+    # SET_METHOD_TEST_CASES = {
+    #     'list': cartesian_prod( [[(2,3),(3,2)],(2,2),[(2,1),[(2,2),(3,2)]]],
+    #                             HASH_SET_VALUES_TUPLES,
+    #                           ( ((0,),None), ((0,1),None), ((1,),None), ((2,1),None), ((2,1,1),None),
+    #                             ((3,), 'key_error'), ((2,0,0),AttributeError), ((), 'key_error') ) ),
+    #     'dict': cartesian_prod( {'a':{'a':(2,1),'c':(2,2)},'b':{'d':(3,2),'b':(2,2)}},
+    #                             HASH_SET_VALUES_TUPLES,
+    #                           ( (('a',), None), (('b',), None), (('c',), None), (('a','a'),None), (('a','e'),None),
+    #                             ((0,), None), ((0,0), 'key_error'),  (('a','a','a'), AttributeError), ((), 'key_error') ) ),
+    #     'mixed': cartesian_prod( [{'a':[(2,2),{'c':(2,1)},(1,2)],'b':(2,1)},{'b':(2,1),'c':[(2,2),(1,2)]}], 
+    #                              HASH_SET_VALUES_TUPLES,
+    #                           ( ((0,), None), ((1,), None), ((0,'a'), None), ((0,'a',1,'c'), None), ((1,'c',0), None),
+    #                             ((0,'b'), None), ((0,'a',3), 'key_error'), ((), 'key_error') ) )
+    # }
+    # SET_METHOD_TEST_CASES = utils.group_first_n_params(SET_METHOD_TEST_CASES, n=2)
+    # @pytest.mark.parametrize('contents_list, key_iterable, exception', utils.unpack_test_cases(SET_METHOD_TEST_CASES), 
+    #                                                                    ids=utils.unpack_test_ids(SET_METHOD_TEST_CASES), 
+    #                                                                    indirect=['contents_list'])
+    # def test_set_and_copy_method(self, contents_list, key_iterable, exception):
         
-        contents, new_val_contents = contents_list
+    #     contents, new_val_contents = contents_list
         
-        if exception is None:
-          contents_copy = deepcopy(contents)
-          utils.set_contents_item(contents_copy, key_iterable, new_val_contents)
+    #     if exception is None:
+    #       contents_copy = deepcopy(contents)
+    #       utils.set_contents_item(contents_copy, key_iterable, new_val_contents)
 
-          # Test two independent sets of conditions:
-          #    1. key_iterables passed as unpacked tuple vs passed as packed tuple
-          #    2. new_val is an arraytainer vs new_val is not an arraytainer 
-          # If contents is just a lone array, do NOT test situation where new_val_contents is converted
-          # to arraytainer, since doing so will place array in a list:
-          val_is_container_tests = (False, True) if not utils.is_array(new_val_contents) else (False,)
-          for unpack_keys, val_is_container in product((False, True), val_is_container_tests):
-            arraytainer = self.container_class(contents)
-            arraytainer_copy = arraytainer.copy()
-            new_val = self.container_class(new_val_contents) if val_is_container else new_val_contents
-            if unpack_keys:
-              arraytainer.set(new_val, *key_iterable)
-            else:
-              arraytainer.set(new_val, key_iterable)
-            utils.assert_equal_values(arraytainer.unpacked, contents_copy)
-            utils.assert_same_types(arraytainer, self.container_class(contents_copy))
-            # Esure setting has not affected copy of arraytainer:
-            utils.assert_equal_values(arraytainer_copy.unpacked, contents)
+    #       # Test two independent sets of conditions:
+    #       #    1. key_iterables passed as unpacked tuple vs passed as packed tuple
+    #       #    2. new_val is an arraytainer vs new_val is not an arraytainer 
+    #       # If contents is just a lone array, do NOT test situation where new_val_contents is converted
+    #       # to arraytainer, since doing so will place array in a list:
+    #       val_is_container_tests = (False, True) if not utils.is_array(new_val_contents) else (False,)
+    #       for unpack_keys, val_is_container in product((False, True), val_is_container_tests):
+    #         arraytainer = self.container_class(contents)
+    #         arraytainer_copy = arraytainer.copy()
+    #         new_val = self.container_class(new_val_contents) if val_is_container else new_val_contents
+    #         if unpack_keys:
+    #           arraytainer.set(new_val, *key_iterable)
+    #         else:
+    #           arraytainer.set(new_val, key_iterable)
+    #         utils.assert_equal_values(arraytainer.unpacked, contents_copy)
+    #         utils.assert_same_types(arraytainer, self.container_class(contents_copy))
+    #         # Esure setting has not affected copy of arraytainer:
+    #         utils.assert_equal_values(arraytainer_copy.unpacked, contents)
         
-        else:
-          arraytainer = self.container_class(contents)
-          # Throw exception when new_val is a list/dict and when container
-          for new_val in (new_val_contents, self.container_class(new_val_contents)):
-            self.assert_exception(lambda arraytainer, new_val, key_iterable : arraytainer.set(new_val, *key_iterable),
-                                  exception, arraytainer, new_val, key_iterable)
+    #     else:
+    #       arraytainer = self.container_class(contents)
+    #       # Throw exception when new_val is a list/dict and when container
+    #       for new_val in (new_val_contents, self.container_class(new_val_contents)):
+    #         self.assert_exception(lambda arraytainer, new_val, key_iterable : arraytainer.set(new_val, *key_iterable),
+    #                               exception, arraytainer, new_val, key_iterable)
 
     # APPEND_METHOD_TEST_CASES
     # def test_append_method(self, contents, key_iterable, new_value, exception):
